f4372e65017811057683e6a9524c20e496a519da
Aisha Tammy <gentoo@aisha.cc>
2020-11-17 18:57:12 +0000

    simplify blas.f90

--- DEBUG/blas.f90
+++ DEBUG/blas.f90
@@ -3,37 +3,22 @@
       IMPLICIT NONE
 
       DOUBLE PRECISION ALPHA, BETA
-      INTEGER          M, K, N, I, J
-      PARAMETER        (M=1500, K=1500, N=1500)
-      DOUBLE PRECISION A(M,K), B(K,N), C(M,N)
+      INTEGER          N, I, J
+      PARAMETER        (N=1500)
+      DOUBLE PRECISION A(N,N), B(N,N), C(N,N)
 
-10    FORMAT(a,I5,a,I5,a,I5,a,I5,a)
       ALPHA = 1.0 
       BETA = 0.0
 
-      DO I = 1, M
-        DO J = 1, K
-          A(I,J) = (I-1) * K + J
-        END DO
-      END DO
-
-      DO I = 1, K
+      DO I = 1, N
         DO J = 1, N
+          A(I,J) = (I-1) * N + J
           B(I,J) = -((I-1) * N + J)
-        END DO
-      END DO
-
-      DO I = 1, M
-        DO J = 1, N
           C(I,J) = 0.0
         END DO
       END DO
 
-      CALL DGEMM('N','N',M,N,K,ALPHA,A,M,B,K,BETA,C,M)
-
- 20   FORMAT(6(F12.0,1x))
-
- 30   FORMAT(6(ES12.4,1x))
+      CALL DGEMM('N','N',N,N,N,ALPHA,A,N,B,N,BETA,C,N)
 
       STOP 
 
